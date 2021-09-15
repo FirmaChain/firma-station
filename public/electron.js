c78c5ac63dda7e4e6e429108f8b7c6f1f1a609e9
chocoSprite <chocosprite21@gmail.com>
2021-09-15 19:42:16 +0900

    update : change resolution for desktop app

--- public/electron.js
+++ public/electron.js
@@ -1,15 +1,32 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, shell } = require("electron");
+const electron = require("electron");
+const path = require("path");
 
 function initialize() {
   function createWindow() {
+    const size = electron.screen.getPrimaryDisplay().workAreaSize;
+    const originWidth = size.width;
+    const width = originWidth > 1080 ? parseInt(1080 + (originWidth - 1080) * 0.5) : originWidth;
+    const height = parseInt(width / (1920 / 1080));
+
     const windowOptions = {
-      width: 1600,
-      height: 900,
+      minWidth: width,
+      minHeight: height,
+      width: width,
+      height: height,
       title: app.getName(),
+      titleBarStyle: "hiddenInset",
+      webPreferences: {
+        nodeIntegration: false,
+        enableRemoteModule: false,
+        nativeWindowOpen: true,
+        preload: path.resolve(__dirname, "preload.js"),
+      },
+      resizable: true,
     };
     mainWindow = new BrowserWindow(windowOptions);
     mainWindow.setMenu(null);
-    mainWindow.loadURL("http://localhost:3001");
+    mainWindow.loadURL("http://localhost:3000");
 
     mainWindow.once("ready-to-show", () => {
       mainWindow.show();
