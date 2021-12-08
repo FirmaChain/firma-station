576ef6a75108ed0e7d3e288784a4fd75f610e39c
chocoSprite <chocosprite21@gmail.com>
2021-12-08 22:01:02 +0900

    update: coin price

--- src/organisms/accounts/accountCard.tsx
+++ src/organisms/accounts/accountCard.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import numeral from "numeral";
 import { useSelector } from "react-redux";
-import { convertNumber } from "../../utils/common";
+import axios from "axios";
 
+import { convertNumber } from "../../utils/common";
 import { rootState } from "../../redux/reducers";
 
 import theme from "../../themes";
@@ -11,7 +12,16 @@ import { AddressTitleTypo, UsdTypo, UserBalanceTypo, PriceTypo } from "./styles"
 
 const AccountCard = () => {
   const { balance } = useSelector((state: rootState) => state.user);
-  const currentUSDPrice = 0.21;
+  const [currentUSDPrice, setPrice] = useState(0.0);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.coingecko.com/api/v3/simple/price?ids=upbit,firmachain&vs_currencies=usd`)
+      .then((res) => {
+        setPrice(Math.round(res.data.firmachain.usd * 100) / 100);
+      })
+      .catch((e) => {});
+  }, []);
 
   return (
     <BlankCard bgColor={theme.colors.mainblue} height="130px" backgroundLogo={true}>
