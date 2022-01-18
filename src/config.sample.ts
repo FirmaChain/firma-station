91e28b8f276deafff6a48913899234e3708a1767
chocoSprite <chocosprite21@gmail.com>
2022-01-18 14:23:42 +0900

    update: added config sample

---
+++ src/config.sample.ts
@@ -0,0 +1,12 @@
+import { FirmaConfig } from "@firmachain/firma-js";
+
+export const NETWORK_INFO_LIST = [];
+
+export const FAUCET_URI = "";
+export const EXPLORER_URI = "";
+
+export const GRAPHQL_CONFIG = {
+  URI: "http://:8080/v1/graphql",
+};
+
+export const FIRMACHAIN_CONFIG = FirmaConfig.TestNetConfig;
