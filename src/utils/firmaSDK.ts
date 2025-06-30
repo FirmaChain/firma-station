e4cbb5db9a6dfec45e7b9c152ca538b8a65151e1
LarryKim <rlatkdgud98@naver.com>
2025-06-30 12:35:37 +0900

    refactor: replace Long type with BigInt

--- src/utils/firmaSDK.ts
+++ src/utils/firmaSDK.ts
@@ -4,7 +4,7 @@ import TransportHID from '@ledgerhq/hw-transport-webhid';
 
 import { CHAIN_CONFIG, IBC_CONFIG } from '../config';
 import { convertToUTokenStringFromToken, getDefaultGas, getFeesFromGas, isElectron, isExternalConnect } from './common';
-import Long from 'long';
+// import Long from 'long';
 
 declare global {
   interface Window {
@@ -163,16 +163,14 @@ const FirmaSDKInternal = ({ isLedger, isMobileApp, getDecryptPrivateKey }: any) 
     const convertAmount = convertToUTokenStringFromToken(amount, decimal);
     const clientState = await firmaSDK.Ibc.getClientState(channel, port);
     const timeStamp = (Date.now() + 600000).toString() + '000000';
-    const timeoutTimeStamp = Long.fromString(timeStamp, true);
+    const timeoutTimeStamp = BigInt(timeStamp); // Long.fromString(timeStamp, true)
     const height = {
-      revisionHeight: Long.fromString(
-        clientState.identified_client_state.client_state.latest_height.revision_height,
-        true
-      ).add(Long.fromNumber(1000)),
-      revisionNumber: Long.fromString(
-        clientState.identified_client_state.client_state.latest_height.revision_number,
-        true
-      ),
+      revisionHeight:
+        BigInt(clientState.identified_client_state.client_state.latest_height.revision_height) + BigInt(1000),
+      //   Long.fromString(clientState.identified_client_state.client_state.latest_height.revision_height, true)
+      //     .add(Long.fromNumber(1000))
+      revisionNumber: BigInt(clientState.identified_client_state.client_state.latest_height.revision_number),
+      //   Long.fromString(clientState.identified_client_state.client_state.latest_height.revision_number, true)
     };
 
     const result = await firmaSDK.Ibc.transfer(
@@ -217,16 +215,14 @@ const FirmaSDKInternal = ({ isLedger, isMobileApp, getDecryptPrivateKey }: any) 
     const convertAmount = convertToUTokenStringFromToken(amount, decimal);
     const clientState = await firmaSDK.Ibc.getClientState(channel, port);
     const timeStamp = (Date.now() + 600000).toString() + '000000';
-    const timeoutTimeStamp = Long.fromString(timeStamp, true);
+    const timeoutTimeStamp = BigInt(timeStamp); // Long.fromString(timeStamp, true)
     const height = {
-      revisionHeight: Long.fromString(
-        clientState.identified_client_state.client_state.latest_height.revision_height,
-        true
-      ).add(Long.fromNumber(1000)),
-      revisionNumber: Long.fromString(
-        clientState.identified_client_state.client_state.latest_height.revision_number,
-        true
-      ),
+      revisionHeight:
+        BigInt(clientState.identified_client_state.client_state.latest_height.revision_height) + BigInt(1000),
+      //   Long.fromString(clientState.identified_client_state.client_state.latest_height.revision_height, true)
+      //     .add(Long.fromNumber(1000))
+      revisionNumber: BigInt(clientState.identified_client_state.client_state.latest_height.revision_number),
+      //   Long.fromString(clientState.identified_client_state.client_state.latest_height.revision_number, true)
     };
 
     try {
