9f0465681d6833581876a3d6ead22c062f29358b
LarryKim <rlatkdgud98@naver.com>
2025-07-03 14:32:20 +0900

    chore: update repo config
    - Remove duplicated modules / un-used modules
    - Recommend re-install dependency
    - Add preview setting in vite.config. try `PORT=3010 yarn run start`
    - Remove yarn install-state

--- vite.config.ts
+++ vite.config.ts
@@ -6,58 +6,64 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import packageJson from './package.json';
 
 const minimizeObfuscatorConfig = {
-	autoExcludeNodeModules: true,
-	threadPool: { enable: true, size: 4 },
+  autoExcludeNodeModules: true,
+  threadPool: { enable: true, size: 4 },
 
-	stringArray: true,
-	stringArrayEncoding: ['base64', 'rc4'],
-	stringArrayThreshold: 1,
-	splitStrings: true,
-	splitStringsChunkLength: 3,
-	transformObjectKeys: true,
-	unicodeEscapeSequence: true,
+  stringArray: true,
+  stringArrayEncoding: ['base64', 'rc4'],
+  stringArrayThreshold: 1,
+  splitStrings: true,
+  splitStringsChunkLength: 3,
+  transformObjectKeys: true,
+  unicodeEscapeSequence: true,
 
-	numbersToExpressions: true,
+  numbersToExpressions: true,
 
-	compact: true,
-	controlFlowFlattening: true,
-	controlFlowFlatteningThreshold: 1,
-	deadCodeInjection: true,
-	deadCodeInjectionThreshold: 1,
-	debugProtection: true,
-	debugProtectionInterval: true,
-	disableConsoleOutput: true,
-	identifierNamesGenerator: 'hexadecimal',
-	log: false,
-	renameGlobals: false,
-	selfDefending: true,
-	simplify: true,
+  compact: true,
+  controlFlowFlattening: true,
+  controlFlowFlatteningThreshold: 1,
+  deadCodeInjection: true,
+  deadCodeInjectionThreshold: 1,
+  debugProtection: true,
+  debugProtectionInterval: true,
+  disableConsoleOutput: true,
+  identifierNamesGenerator: 'hexadecimal',
+  log: false,
+  renameGlobals: false,
+  selfDefending: true,
+  simplify: true
 };
 
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
-	return {
-		plugins: [
-			react(),
-			nodePolyfills(),
-			tsconfigPaths(),
-			...(env.NODE_ENV === 'production' ? [vitePluginBundleObfuscator(minimizeObfuscatorConfig)] : []),
-		],
+  return {
+    plugins: [
+      react(),
+      nodePolyfills(),
+      tsconfigPaths(),
+      ...(env.NODE_ENV === 'production' ? [vitePluginBundleObfuscator(minimizeObfuscatorConfig)] : [])
+    ],
 
-		server: {
-			port: 3000,
-			host: true,
-			strictPort: false,
-		},
+    server: {
+      port: env.PORT || 3000,
+      host: true,
+      strictPort: false
+    },
 
-		build: {
-			outDir: 'build',
-			sourcemap: false,
-		},
+    preview: {
+      port: env.PORT || 3000,
+      host: true,
+      strictPort: false
+    },
 
-		define: {
-			'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
-		},
-	};
+    build: {
+      outDir: 'build',
+      sourcemap: false
+    },
+
+    define: {
+      'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version)
+    }
+  };
 });
