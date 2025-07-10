203e7b592f589f242efd9842b4cb2e96870b0dc6
LarryKim <rlatkdgud98@naver.com>
2025-07-10 10:45:01 +0900

    fix: add submit proposal tag in history page

--- src/constants/transactions.ts
+++ src/constants/transactions.ts
@@ -23,7 +23,7 @@ const TYPE_COLORS: IKeyValue = {
   seventeen: '#E99E8E',
   eighteen: '#F0A479',
   nineteen: '#D37763',
-  twenty: '#D9C788',
+  twenty: '#D9C788'
 };
 
 const LABELS: IKeyValue = {
@@ -97,215 +97,220 @@ const LABELS: IKeyValue = {
   txCosmwasmExecuteContractLabel: 'CosmWasm ExecuteContract',
   txCosmwasmMigrateContractLabel: 'CosmWasm MigrateContract',
   txCosmwasmUpdateAdminLabel: 'CosmWasm UpdateAdmin',
-  txCosmwasmClearAdminLabel: 'CosmWasm ClearAdmin',
+  txCosmwasmClearAdminLabel: 'CosmWasm ClearAdmin'
 };
 
 export const TRANSACTION_TYPE_MODEL: IKeyValue = {
   Unknown: {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txUnknownLabel'],
+    tagDisplay: LABELS['txUnknownLabel']
   },
   // ========================
   // staking
   // ========================
   '/cosmos.staking.v1beta1.MsgDelegate': {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txDelegateLabel'],
+    tagDisplay: LABELS['txDelegateLabel']
   },
   '/cosmos.staking.v1beta1.MsgBeginRedelegate': {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txRedelegateLabel'],
+    tagDisplay: LABELS['txRedelegateLabel']
   },
   '/cosmos.staking.v1beta1.MsgUndelegate': {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txUndelegateLabel'],
+    tagDisplay: LABELS['txUndelegateLabel']
   },
   '/cosmos.staking.v1beta1.MsgCreateValidator': {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txCreateValidatorLabel'],
+    tagDisplay: LABELS['txCreateValidatorLabel']
   },
   '/cosmos.staking.v1beta1.MsgEditValidator': {
     tagTheme: TYPE_COLORS['one'],
-    tagDisplay: LABELS['txEditValidatorLabel'],
+    tagDisplay: LABELS['txEditValidatorLabel']
   },
   // ========================
   // bank
   // ========================
   '/cosmos.bank.v1beta1.MsgSend': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txSendLabel'],
+    tagDisplay: LABELS['txSendLabel']
   },
   '/cosmos.bank.v1beta1.MsgMultiSend': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txMultisendLabel'],
+    tagDisplay: LABELS['txMultisendLabel']
   },
   // ========================
   // crisis
   // ========================
   '/cosmos.crisis.v1beta1.MsgVerifyInvariant': {
     tagTheme: TYPE_COLORS['three'],
-    tagDisplay: LABELS['txVerifyInvariantLabel'],
+    tagDisplay: LABELS['txVerifyInvariantLabel']
   },
   // ========================
   // slashing
   // ========================
   '/cosmos.slashing.v1beta1.MsgUnjail': {
     tagTheme: TYPE_COLORS['five'],
-    tagDisplay: LABELS['txUnjailLabel'],
+    tagDisplay: LABELS['txUnjailLabel']
   },
   // ========================
   // distribution
   // ========================
   '/cosmos.distribution.v1beta1.MsgFundCommunityPool': {
     tagTheme: TYPE_COLORS['six'],
-    tagDisplay: LABELS['txFundLabel'],
+    tagDisplay: LABELS['txFundLabel']
   },
   '/cosmos.distribution.v1beta1.MsgSetWithdrawAddress': {
     tagTheme: TYPE_COLORS['six'],
-    tagDisplay: LABELS['txsetRewardAddressLabel'],
+    tagDisplay: LABELS['txsetRewardAddressLabel']
   },
   '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward': {
     tagTheme: TYPE_COLORS['six'],
-    tagDisplay: LABELS['txWithdrawRewardLabel'],
+    tagDisplay: LABELS['txWithdrawRewardLabel']
   },
   '/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission': {
     tagTheme: TYPE_COLORS['six'],
-    tagDisplay: LABELS['txWithdrawCommissionLabel'],
+    tagDisplay: LABELS['txWithdrawCommissionLabel']
   },
   // ========================
   // governance
   // ========================
   '/cosmos.gov.v1beta1.MsgDeposit': {
     tagTheme: TYPE_COLORS['seven'],
-    tagDisplay: LABELS['txDepositLabel'],
+    tagDisplay: LABELS['txDepositLabel']
   },
   '/cosmos.gov.v1beta1.MsgVote': {
     tagTheme: TYPE_COLORS['seven'],
-    tagDisplay: LABELS['txVoteLabel'],
+    tagDisplay: LABELS['txVoteLabel']
   },
   '/cosmos.gov.v1beta1.MsgSubmitProposal': {
     tagTheme: TYPE_COLORS['seven'],
-    tagDisplay: LABELS['txSubmitProposalLabel'],
+    tagDisplay: LABELS['txSubmitProposalLabel']
+  },
+  //? Added since v0.5.0
+  '/cosmos.gov.v1.MsgSubmitProposal': {
+    tagTheme: TYPE_COLORS['seven'],
+    tagDisplay: LABELS['txSubmitProposalLabel']
   },
   '/cosmos.gov.v1.MsgCancelProposal': {
     tagTheme: TYPE_COLORS['seven'],
-    tagDisplay: LABELS['txCancelProposalLabel'],
+    tagDisplay: LABELS['txCancelProposalLabel']
   },
   // ========================
   // ibc client
   // ========================
   '/ibc.core.client.v1.MsgCreateClient': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCreateClientLabel'],
+    tagDisplay: LABELS['txCreateClientLabel']
   },
   '/ibc.core.client.v1.MsgUpdateClient': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txUpdateClientLabel'],
+    tagDisplay: LABELS['txUpdateClientLabel']
   },
   '/ibc.core.client.v1.MsgUpgradeClient': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txUpgradeClientLabel'],
+    tagDisplay: LABELS['txUpgradeClientLabel']
   },
   '/ibc.core.client.v1.MsgSubmitMisbehaviour': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txSubmitMisbehaviourLabel'],
+    tagDisplay: LABELS['txSubmitMisbehaviourLabel']
   },
   '/ibc.core.client.v1.Height': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txHeightLabel'],
+    tagDisplay: LABELS['txHeightLabel']
   },
   // ========================
   // ibc channel
   // ========================
   '/ibc.core.channel.v1.MsgRecvPacket': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txRecvPacketLabel'],
+    tagDisplay: LABELS['txRecvPacketLabel']
   },
   '/ibc.core.channel.v1.Channel': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelLabel'],
+    tagDisplay: LABELS['txChannelLabel']
   },
   '/ibc.core.channel.v1.Counterparty': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCounterpartyLabel'],
+    tagDisplay: LABELS['txCounterpartyLabel']
   },
   '/ibc.core.channel.v1.Packet': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txPacketLabel'],
+    tagDisplay: LABELS['txPacketLabel']
   },
   '/ibc.core.channel.v1.MsgAcknowledgement': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txAcknowledgementLabel'],
+    tagDisplay: LABELS['txAcknowledgementLabel']
   },
   '/ibc.core.channel.v1.MsgChannelCloseConfirm': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelCloseConfirmLabel'],
+    tagDisplay: LABELS['txChannelCloseConfirmLabel']
   },
   '/ibc.core.channel.v1.MsgChannelCloseInit': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelCloseInitLabel'],
+    tagDisplay: LABELS['txChannelCloseInitLabel']
   },
   '/ibc.core.channel.v1.MsgChannelOpenAck': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelOpenAckLabel'],
+    tagDisplay: LABELS['txChannelOpenAckLabel']
   },
   '/ibc.core.channel.v1.MsgChannelOpenConfirm': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelOpenConfirmLabel'],
+    tagDisplay: LABELS['txChannelOpenConfirmLabel']
   },
   '/ibc.core.channel.v1.MsgChannelOpenInit': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelOpenInitLabel'],
+    tagDisplay: LABELS['txChannelOpenInitLabel']
   },
   '/ibc.core.channel.v1.MsgChannelOpenTry': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txChannelOpenTryLabel'],
+    tagDisplay: LABELS['txChannelOpenTryLabel']
   },
   '/ibc.core.channel.v1.MsgTimeout': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txTimeoutLabel'],
+    tagDisplay: LABELS['txTimeoutLabel']
   },
   '/ibc.core.channel.v1.MsgTimeoutOnClose': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txTimeoutOnCloseLabel'],
+    tagDisplay: LABELS['txTimeoutOnCloseLabel']
   },
   // ========================
   // ibc connection
   // ========================
   '/ibc.core.connection.v1.MsgConnectionOpenAck': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txConnectionOpenAckLabel'],
+    tagDisplay: LABELS['txConnectionOpenAckLabel']
   },
   '/ibc.core.connection.v1.MsgConnectionOpenConfirm': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txConnectionOpenConfirmLabel'],
+    tagDisplay: LABELS['txConnectionOpenConfirmLabel']
   },
   '/ibc.core.connection.v1.MsgConnectionOpenInit': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txConnectionOpenInitLabel'],
+    tagDisplay: LABELS['txConnectionOpenInitLabel']
   },
   '/ibc.core.connection.v1.MsgConnectionOpenTry': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txConnectionOpenTryLabel'],
+    tagDisplay: LABELS['txConnectionOpenTryLabel']
   },
   '/ibc.core.connection.v1.ConnectionEnd': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txConnectionEndLabel'],
+    tagDisplay: LABELS['txConnectionEndLabel']
   },
   '/ibc.core.connection.v1.Counterparty': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCounterpartyLabel'],
+    tagDisplay: LABELS['txCounterpartyLabel']
   },
   '/ibc.core.connection.v1.Version': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txVersionLabel'],
+    tagDisplay: LABELS['txVersionLabel']
   },
   // ========================
   // ibc transfer
   // ========================
   '/ibc.applications.transfer.v1.MsgTransfer': {
     tagTheme: TYPE_COLORS['ten'],
-    tagDisplay: LABELS['txTransferLabel'],
+    tagDisplay: LABELS['txTransferLabel']
   },
 
   // ========================
@@ -313,83 +318,83 @@ export const TRANSACTION_TYPE_MODEL: IKeyValue = {
   // ========================
   '/firmachain.firmachain.nft.MsgMint': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txNFTMintLabel'],
+    tagDisplay: LABELS['txNFTMintLabel']
   },
   '/firmachain.firmachain.nft.MsgTransfer': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txNFTTransferLabel'],
+    tagDisplay: LABELS['txNFTTransferLabel']
   },
   '/firmachain.firmachain.nft.MsgBurn': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txNFTBurnLabel'],
+    tagDisplay: LABELS['txNFTBurnLabel']
   },
   '/firmachain.firmachain.contract.MsgAddContractLog': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txAddContractLogLabel'],
+    tagDisplay: LABELS['txAddContractLogLabel']
   },
   '/firmachain.firmachain.contract.MsgCreateContractFile': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txCreateContractFileLabel'],
+    tagDisplay: LABELS['txCreateContractFileLabel']
   },
   '/cosmos.feegrant.v1beta1.MsgGrantAllowance': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txFeegrantGrantLabel'],
+    tagDisplay: LABELS['txFeegrantGrantLabel']
   },
   '/cosmos.feegrant.v1beta1.MsgRevokeAllowance': {
     tagTheme: TYPE_COLORS['three'],
-    tagDisplay: LABELS['txFeegrantRevokeLabel'],
+    tagDisplay: LABELS['txFeegrantRevokeLabel']
   },
   '/cosmos.authz.v1beta1.MsgGrant': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txAuthzGrantLabel'],
+    tagDisplay: LABELS['txAuthzGrantLabel']
   },
   '/firmachain.firmachain.token.MsgCreateToken': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txTokenCreateLabel'],
+    tagDisplay: LABELS['txTokenCreateLabel']
   },
   '/firmachain.firmachain.token.MsgMint': {
     tagTheme: TYPE_COLORS['two'],
-    tagDisplay: LABELS['txTokenMintLabel'],
+    tagDisplay: LABELS['txTokenMintLabel']
   },
   '/firmachain.firmachain.token.MsgBurn': {
     tagTheme: TYPE_COLORS['four'],
-    tagDisplay: LABELS['txTokenBurnLabel'],
+    tagDisplay: LABELS['txTokenBurnLabel']
   },
   '/firmachain.firmachain.token.MsgUpdateTokenURI': {
     tagTheme: TYPE_COLORS['three'],
-    tagDisplay: LABELS['txTokenUpdateURILabel'],
+    tagDisplay: LABELS['txTokenUpdateURILabel']
   },
   '/cosmos.authz.v1beta1.MsgRevoke': {
     tagTheme: TYPE_COLORS['three'],
-    tagDisplay: LABELS['txAuthzRevokeLabel'],
+    tagDisplay: LABELS['txAuthzRevokeLabel']
   },
   '/cosmos.authz.v1beta1.MsgExec': {
     tagTheme: TYPE_COLORS['three'],
-    tagDisplay: LABELS['txAuthzExecLabel'],
+    tagDisplay: LABELS['txAuthzExecLabel']
   },
 
   '/cosmwasm.wasm.v1.MsgStoreCode': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmStoreCodeLabel'],
+    tagDisplay: LABELS['txCosmwasmStoreCodeLabel']
   },
   '/cosmwasm.wasm.v1.MsgInstantiateContract': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmInstantiateContractLabel'],
+    tagDisplay: LABELS['txCosmwasmInstantiateContractLabel']
   },
   '/cosmwasm.wasm.v1.MsgExecuteContract': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmExecuteContractLabel'],
+    tagDisplay: LABELS['txCosmwasmExecuteContractLabel']
   },
   '/cosmwasm.wasm.v1.MsgMigrateContract': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmMigrateContractLabel'],
+    tagDisplay: LABELS['txCosmwasmMigrateContractLabel']
   },
   '/cosmwasm.wasm.v1.MsgUpdateAdmin': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmUpdateAdminLabel'],
+    tagDisplay: LABELS['txCosmwasmUpdateAdminLabel']
   },
   '/cosmwasm.wasm.v1.MsgClearAdmin': {
     tagTheme: TYPE_COLORS['nine'],
-    tagDisplay: LABELS['txCosmwasmClearAdminLabel'],
-  },
+    tagDisplay: LABELS['txCosmwasmClearAdminLabel']
+  }
 };
